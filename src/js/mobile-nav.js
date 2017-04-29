e329bca06e9327af30d7ff4723fee0fdba0912a7
Michael Misciagna <mmisciagna@google.com>
2017-04-28 19:54:17 -0700

    Animate mobile nav items with JS instead of scss

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -3,11 +3,15 @@ const utils = require('./utils');
 
 const Selectors = {
   CLOSE: '.js-close-mobile-nav',
+  ITEMS: '.js-mobile-nav-item',
   REVEAL: '.js-reveal-mobile-nav',
   ROOT: '.js-mobile-nav'
 };
 
-const REVEAL_CLASS_NAME = 'mobile-nav--is-visible';
+const ClassNames = {
+  ANIMATE_ITEM: 'mobile-nav__item--animate-in',
+  REVEAL: 'mobile-nav--is-visible'
+};
 
 const ARIA_ATTRIBUTE = 'aria-hidden';
 
@@ -15,17 +19,31 @@ const ARIA_ATTRIBUTE = 'aria-hidden';
 class MobileNav {
   constructor() {
     this.root = document.querySelector(Selectors.ROOT);
+    this.items = this.root.querySelectorAll(Selectors.ITEMS);
 
-    // Reveals the mobile nav.
-    utils.delegate(document.body, Selectors.REVEAL, 'click', () => {
-      this.root.classList.add(REVEAL_CLASS_NAME);
-      this.root.setAttribute(ARIA_ATTRIBUTE, false);
-    });
+    utils.delegate(document.body, Selectors.REVEAL, 'click',
+        this.reveal.bind(this));
+    utils.delegate(document.body, Selectors.CLOSE, 'click',
+        this.close.bind(this));
+  }
 
-    // Closes the mobile nav.
-    utils.delegate(document.body, Selectors.CLOSE, 'click', () => {
-      this.root.classList.remove(REVEAL_CLASS_NAME);
-      this.root.setAttribute(ARIA_ATTRIBUTE, true);
+  reveal() {
+    this.root.classList.add(ClassNames.REVEAL);
+    this.root.setAttribute(ARIA_ATTRIBUTE, false);
+    let index = 0;
+
+    const interval = setInterval(() => {
+      this.items[index++].classList.add(ClassNames.ANIMATE_ITEM);
+      if (index === this.items.length) clearInterval(interval);
+    }, 100);
+  }
+
+  close() {
+    this.root.classList.remove(ClassNames.REVEAL);
+    this.root.setAttribute(ARIA_ATTRIBUTE, true);
+
+    this.items.forEach((item) => {
+      item.classList.remove(ClassNames.ANIMATE_ITEM);
     });
   }
 }
